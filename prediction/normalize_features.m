547058cbde1df298d7968233173ba9768a1a0eed
Theja Tulabandhula <theja@mit.edu>
2014-02-10 12:28:23 -0500

    refactored normalize_features.m, need to reuse this in get_decision_data.m [futurework]

--- prediction/normalize_features.m
+++ prediction/normalize_features.m
@@ -1,12 +1,13 @@
-function [X_trn,X_val,avg_X_trn,var_X_trn] = normalize_features(xTrain_o,xTest_o)
+function [X_trn_norm,X_val_norm,avg_X_trn,var_X_trn] = normalize_features(X_trn,X_val)
 %de-mean and normalize the data using training variance.
 
-n_train   = size(xTrain_o,1);
-var_X_trn = var(xTrain_o);
-avg_X_trn = mean(xTrain_o);
+n_train   = size(X_trn,1);
+n_val     = size(X_val,1);
+var_X_trn = var(X_trn);
+avg_X_trn = mean(X_trn);
 
-X_trn = (xTrain_o - repmat(avg_X_trn,n_train,1))./repmat(sqrt(var_X_trn),n_train,1);
-X_val = (xTest_o - repmat(avg_X_trn,n_train,1))./repmat(sqrt(var_X_trn),n_train,1);
+X_trn_norm = (X_trn - repmat(avg_X_trn,n_train,1))./repmat(sqrt(var_X_trn),n_train,1);
+X_val_norm = (X_val - repmat(avg_X_trn,n_val,1))./repmat(sqrt(var_X_trn),n_val,1);
 
-X_trn = [X_trn ones(size(X_trn,1),1)]; %add the last feature which is all ones
-X_val = [X_val ones(size(X_val,1),1)];
+X_trn_norm = [X_trn_norm ones(n_train,1)]; %add the last feature which is all ones
+X_val_norm = [X_val_norm ones(n_val,1)];
