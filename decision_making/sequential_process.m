99676f89cec298b8b5f8862247724bd7f2f0839d
Theja Tulabandhula <theja@mit.edu>
2014-02-11 19:00:49 -0500

    [minor] commented fprintfs, need to add a debug flag

--- decision_making/sequential_process.m
+++ decision_making/sequential_process.m
@@ -5,7 +5,7 @@ function [sequential] = sequential_process(param)
             cvalidated_model('LogReg',param.C2_coeff_range,param.n_folds,param.n_repeats,...
                                 param.X_trn,param.Y_trn,param.X_val,0,0.1);
 [train_auc,test_auc] = performance_of_learning(param.Y_trn,Y_hat_trn,param.Y_val,Y_hat_val);
-
+% fprintf('Finished sequential process CV-logistic regression.');
 
 % Obtain probabilities q on decision problem data which is then fed to wTRP solver.
 q = get_predicted_probabilities(param.unLabeled, param.n_features, lambda_model, param.cost_model_type);
@@ -24,3 +24,5 @@ sequential.regularized_coeff = regularize_coeff;%best regularization coefficient
 sequential.cv_matrix = cv_matrix;       %cross validation matrix
 sequential.train_auc = train_auc;       %training AUC
 sequential.test_auc = test_auc;         %Test/Validation AUC
+
+% fprintf('Finished sequential process.');
