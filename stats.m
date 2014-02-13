5ba4a12c0d78b21ad8ed9cbd0e8df573235e5a99
Theja Tulabandhula <theja@mit.edu>
2014-02-13 11:39:12 -0500

    patching works.

--- stats.m
+++ stats.m
@@ -3,10 +3,11 @@
 
 clc;clear all;close all;
 result_path  = '/Users/theja/Downloads/temp/mloc_results_feb_2014/';%repetition, maybe move to startup? will get cleared unless do addpath
-cost_model_type = 2;
+cost_model_type = 1;
 
 if(cost_model_type==1)
-    load([result_path 'run_2014_02_XX_XXXXhrs_cost_type_1_addendum.mat']);
+    load([result_path 'run_2014_02_12_1900hrs_cost_type_1_addendum.mat'],...
+        'sequential','am_data','n_sample_size_pcts');
     sequential_addendum         = sequential;
     am_data_addendum            = am_data;
     n_sample_size_pcts_addendum = n_sample_size_pcts;
@@ -14,7 +15,8 @@ if(cost_model_type==1)
     [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
         sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
 else
-    load([result_path 'run_2014_02_12_1700hrs_cost_type_2_addendum.mat']);
+    load([result_path 'run_2014_02_12_1700hrs_cost_type_2_addendum.mat'],...
+        'sequential','am_data','n_sample_size_pcts');
     sequential_addendum         = sequential;
     am_data_addendum            = am_data;
     n_sample_size_pcts_addendum = n_sample_size_pcts;
@@ -23,7 +25,7 @@ else
         sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
 end
 
-%%
+
 close all;
 plot_performance(n_sample_size_pcts,n_multirun,sequential,am_data);
 
