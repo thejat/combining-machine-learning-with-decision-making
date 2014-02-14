965b611ae285d6d9616e6ea32b129449e70c103e
Theja Tulabandhula <theja@omegafp.local>
2014-02-13 20:23:51 -0500

    scalbility test with parfor

--- scalability_test.m
+++ scalability_test.m
@@ -13,10 +13,11 @@ decision_nodes_array = [3 5];
 param1 = get_data_given_sample_size(param0,.1);%n_sample_size fixed to 10%    
 param1 = sequential_training(param1); %does a single training for all decision instances
 time_loop_start = datevec(now);
+%%
 parpool
 for j=1:length(decision_nodes_array)
     parfor k=1:param0.n_multirun
-        time_loop(j,k) = datevec(now);
+        time_loop{j,k} = datevec(now);
         %Workflow: Get decision data, do sequential, do simultaneous
         [sequential{j,k},am_data{j,k},param_sample_size{j,k}] = ...
                                     mloc_workflow_wrapper(param1);
@@ -27,7 +28,7 @@ for j=1:length(decision_nodes_array)
         fprintf(['MLOC multirun: j:%2d, k:%2d finished.' ...
             ' Total elapsed time: %4.4f, this instance: %4.4f\n'],...
             j,k,etime(datevec(now),time_loop_start),...
-                etime(datevec(now),time_loop(j,k)));
+                etime(datevec(now),time_loop{j,k}));
     end
     save([param0.result_path 'run_scalability_' ...
         datestr(now,'yyyy_mm_dd_HHMM') 'hrs_cost_type_' ...
