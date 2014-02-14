2ab8b3c19acea1c5445861b06eed12cfc2d6418b
Theja Tulabandhula <theja@mit.edu>
2014-02-14 13:16:56 -0500

    refactored learning performance plotting

--- stats.m
+++ stats.m
@@ -5,29 +5,7 @@ clc;clear all;close all;
 result_path  = '../data/mloc_results_feb_2014/';%repetition, maybe move to startup? will get cleared unless do addpath
 cost_model_type = 1;
 
-if(cost_model_type==1)
-    load([result_path 'run_2014_02_12_1900hrs_cost_type_1_addendum.mat'],...
-        'sequential','am_data','n_sample_size_pcts');
-    sequential_addendum         = sequential;
-    am_data_addendum            = am_data;
-    n_sample_size_pcts_addendum = n_sample_size_pcts;
-    load([result_path 'run_2014_02_10_1500hrs_cost_type_1.mat']);
-    [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
-        sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
-else
-    load([result_path 'run_2014_02_12_1700hrs_cost_type_2_addendum.mat'],...
-        'sequential','am_data','n_sample_size_pcts');
-    sequential_addendum         = sequential;
-    am_data_addendum            = am_data;
-    n_sample_size_pcts_addendum = n_sample_size_pcts;
-    load([result_path 'run_2014_02_12_1000hrs_cost_type_2.mat']);    
-    [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
-        sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
-end
-
-
-close all;
-plot_performance(n_sample_size_pcts,n_multirun,sequential,am_data);
+plot_learning_performance(result_path,cost_model_type);
 
 
 %% AUC of an all -1 guesser: No ordering means bad guesser.
