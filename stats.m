865c9863d1a2fb506357098c02f9ff620f47446d
Theja Tulabandhula <theja@mit.edu>
2014-02-14 14:51:22 -0500

    consolidated plotting between mloc_multirun and scalability_test scripts

--- stats.m
+++ stats.m
@@ -5,12 +5,9 @@ clc;clear all;close all;
 result_path  = '../data/mloc_results_feb_2014/';%repetition, maybe move to startup? will get cleared unless do addpath
 cost_model_type = 1;
 
+
+%% Learning performance
 plot_learning_performance(result_path,cost_model_type);
 
-
-%% AUC of an all -1 guesser: No ordering means bad guesser.
-[baseline_train_auc,baseline_test_auc] = performance_of_learning(...
-                            param1.Y_trn,...
-                            -1*ones(size(param1.Y_trn,1),1),...
-                            param1.Y_val,...
-                            -1*ones(size(param1.Y_trn,1),1))
+%% Scaling performance
+plot_scale_performance(result_path,cost_model_type);
--- stats/plot_learning_performance.m
+++ stats/plot_learning_performance.m
@@ -24,4 +24,12 @@ end
 
 
 close all;
-plot_performance(n_sample_size_pcts,n_multirun,sequential,am_data);
+plot_learning_performance_subroutine(n_sample_size_pcts,n_multirun,sequential,am_data);
+
+
+% AUC of an all -1 guesser: No ordering means bad guesser.
+% [baseline_train_auc,baseline_test_auc] = performance_of_learning(...
+%                             param1.Y_trn,...
+%                             -1*ones(size(param1.Y_trn,1),1),...
+%                             param1.Y_val,...
+%                             -1*ones(size(param1.Y_trn,1),1))
