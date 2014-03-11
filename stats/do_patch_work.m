7d0fa01a69c54d2e632484d0d49ccf5c20f0a3ab
Theja Tulabandhula <theja@mit.edu>
2014-03-11 11:18:38 -0400

    minor

--- stats/do_patch_work.m
+++ stats/do_patch_work.m
@@ -13,7 +13,7 @@ am_data_new = am_data_old;
 for i=1:length(n_sample_size_pcts_addendum)
     clear idx
     idx = find(abs(n_sample_size_pcts_old-n_sample_size_pcts_addendum(i))<10^-5);
-    fprintf('Iserting at row %d\n',idx);
+    fprintf('Inserting at row %d\n',idx);
     for k=1:size(sequential_addendum,2)
         sequential_new{idx,k} = sequential_addendum{i,k};
         am_data_new{idx,k} = am_data_addendum{i,k};
