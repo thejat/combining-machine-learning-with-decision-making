dcc70eca28c6d631f76e291a36c6b1e4247146f6
Theja Tulabandhula <theja@mit.edu>
2014-03-11 14:59:13 -0400

    refactoring and inserting n_multirun into param0

--- stats/plot_learning_performance.m
+++ stats/plot_learning_performance.m
@@ -1,29 +1,11 @@
 function [] = plot_learning_performance(result_path,cost_model_type)
 
 
-%%
-if(cost_model_type==1)
-    load([result_path 'run_2014_02_12_1900hrs_cost_type_1_addendum.mat'],...
-        'sequential','am_data','n_sample_size_pcts');
-    sequential_addendum         = sequential;
-    am_data_addendum            = am_data;
-    n_sample_size_pcts_addendum = n_sample_size_pcts;
-    load([result_path 'run_2014_02_10_1500hrs_cost_type_1.mat']);
-    [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
-        sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
-else
-    load([result_path 'run_2014_02_12_1700hrs_cost_type_2_addendum.mat'],...
-        'sequential','am_data','n_sample_size_pcts');
-    sequential_addendum         = sequential;
-    am_data_addendum            = am_data;
-    n_sample_size_pcts_addendum = n_sample_size_pcts;
-    load([result_path 'run_2014_02_12_1000hrs_cost_type_2.mat']);    
-    [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
-        sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
-end
+[am_data,sequential,param0,n_sample_size_pcts] = do_patch_work_learning(result_path,cost_model_type);
+
 
 %%
-plot_learning_performance_subroutine(n_sample_size_pcts,n_multirun,sequential,am_data,cost_model_type);
+plot_learning_performance_subroutine(n_sample_size_pcts,param0.n_multirun,sequential,am_data,cost_model_type);
 
 
 % AUC of an all -1 guesser: No ordering means bad guesser.
--- stats/plot_train_scale_performance.m
+++ stats/plot_train_scale_performance.m
@@ -2,32 +2,13 @@ function [] = plot_train_scale_performance(result_path,cost_model_type)
 
 
 
-% same as plot_learning_performance.m
-if(cost_model_type==1)
-    load([result_path 'run_2014_02_12_1900hrs_cost_type_1_addendum.mat'],...
-        'sequential','am_data','n_sample_size_pcts');
-    sequential_addendum         = sequential;
-    am_data_addendum            = am_data;
-    n_sample_size_pcts_addendum = n_sample_size_pcts;
-    load([result_path 'run_2014_02_10_1500hrs_cost_type_1.mat']);
-    [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
-        sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
-else
-    load([result_path 'run_2014_02_12_1700hrs_cost_type_2_addendum.mat'],...
-        'sequential','am_data','n_sample_size_pcts');
-    sequential_addendum         = sequential;
-    am_data_addendum            = am_data;
-    n_sample_size_pcts_addendum = n_sample_size_pcts;
-    load([result_path 'run_2014_02_12_1000hrs_cost_type_2.mat']);    
-    [sequential,am_data] = do_patch_work(sequential,am_data,n_sample_size_pcts,...
-        sequential_addendum,am_data_addendum,n_sample_size_pcts_addendum);
-end
+[am_data,sequential,param0,n_sample_size_pcts] = do_patch_work_learning(result_path,cost_model_type);
 
 % Scaling with training data: number of nodes  = 7
 % Time elapsed for different training sample sizes
-time_elapsed = NaN(length(n_sample_size_pcts),n_multirun);
+time_elapsed = NaN(length(n_sample_size_pcts),param0.n_multirun);
 for j=1:length(n_sample_size_pcts)
-    for k = 1:n_multirun
+    for k = 1:param0.n_multirun
         
         if(sequential{j,k}.feasible==0)
             continue;
